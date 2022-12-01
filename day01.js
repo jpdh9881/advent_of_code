2725a0b24270689c77a7938e35d54685c622674b
Joseph Haley <haley.joseph@gmail.com>
2022-12-01 16:32:44 -0500

    Done day 1

---
+++ day01.js
@@ -0,0 +1,37 @@
+import { open } from "node:fs/promises";
+
+let file;
+try {
+  file = await open("./day01_input.txt");
+  const contents = await file.readFile();
+  const depthStrings = contents.toString().split("\n");
+  const depths = depthStrings.map(d => parseInt(d));
+
+  // Part 1
+  let increases = 0;
+  for (let i = 1; i < depths.length; i++) {
+    if (depths[i] > depths[i - 1]) increases++;
+  }
+
+  console.log("# of increases:", increases);
+
+  // Part 2
+  const windowSize = 3;
+  let windowIncreases = 0;
+  for (let i = 0; i < depths.length - windowSize; i++) {
+    const window1 = depths.slice(i, i + windowSize);
+    const window2 = depths.slice(i + 1, i + 1 + windowSize);
+
+    let window1Sum = 0;
+    let window2Sum = 0;
+    for (let i = 0; i < windowSize; i++) {
+      window1Sum += window1[i];
+      window2Sum += window2[i];
+    }
+    if (window2Sum > window1Sum) windowIncreases++;
+  }
+
+  console.log("# of window increases:", windowIncreases);
+} finally {
+  file?.close();
+}
